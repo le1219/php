9fa3038be4b76b682ba09ed74f439146a48e8491
le1219 <lecy1219@gmail.com>
2021-07-20 12:39:57 +0800

    07/20

--- php_ex/e3-3.php
+++ php_ex/e3-3.php
@@ -60,6 +60,58 @@
 
     ?>
 
+    <?php
+    //str_replace(),str_pad(),str_repeat()
+    $str06 = '正規表達式';
+    echo str_replace('達', '了', $str06) . '<br>';
+    echo str_pad('不要', 30, '阿') . '<br>';
+    echo 'y' . str_repeat('e', 5);
+    ?>
+    <hr>
+    <?php
+    //strtolower(),strtoupper
+    $str07_1 = 'HELLO';
+    $str07_2 = 'world';
+    echo strtolower($str07_1) . strtoupper($str07_2);
+    ?>
+
+    <hr>
+    <?php
+    //md5()
+    $strOrigin = 'T2st@localhost';
+    echo '原始資料：' . $strOrigin . '<br>';
+    echo 'md5()加密後：' . md5($strOrigin) . '<br>';
+
+    $strOrigin = 'Test@localhost';
+    echo '修改資料後：' . $strOrigin . '<br>';
+    echo 'md5()加密後：' . md5($strOrigin) . '<br>';
+
+    $strOrigin = 'T2st@localhost';
+    echo '原始資料：' . $strOrigin . '<br>';
+    echo 'md5()加密後：' . md5($strOrigin) . '<br>';
+    ?>
+
+    <hr>
+    <?php
+    //sha1()
+    $strOrigin = 'T2st@localhost';
+    echo '原始資料：' . $strOrigin . '<br>';
+    echo 'sha1()加密後：' . sha1($strOrigin) . '<br>';
+
+    $strOrigin = 'Test@localhost';
+    echo '修改資料後：' . $strOrigin . '<br>';
+    echo 'sha1()加密後：' . sha1($strOrigin) . '<br>';
+
+    $strOrigin = 'T2st@localhost';
+    echo '原始資料：' . $strOrigin . '<br>';
+    echo 'sha1()加密後：' . sha1($strOrigin) . '<br>';
+    ?>
+
+    <hr>
+    <?php
+    //銷毀，釋放變數
+    unset($str01, $str02, $str03, $str04_1, $str04_2, $arr03);
+    ?>
 
 </body>
 
---
+++ php_ex/e5-1.php
@@ -0,0 +1,29 @@
+<?php
+//
+$a = 5;
+if ($a > 0) echo '$a變數的值是正數';
+echo "<br>";
+if ($a > 0) {
+    echo '$a 變數為大於0';
+}
+
+echo "<hr>";
+
+$b = -5;
+if ($b > 0) {
+    echo '$b 大於0';
+} else {
+    echo '小於0';
+}
+echo "<hr>";
+
+$score = 85;
+if ($score >= 60 && $score < 70) {
+    echo '3';
+} elseif ($score >= 70 && $score < 80) {
+    echo "2";
+} elseif ($score >= 80 && $score <= 100) {
+    echo "1";
+} else {
+    echo "bye";
+}
